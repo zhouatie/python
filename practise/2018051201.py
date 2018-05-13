48e87efd6dd563c18eac93af67f3f9afc6108550
zhoust <zhoust@guahao.com>
2018-05-13 17:45:01 +0800

    2018051201

---
+++ practise/2018051201.py
@@ -0,0 +1,120 @@
+# -- coding: utf-8 --
+# message = 'haha'
+# print(message)
+
+# print(32/4)
+
+# arr = ['a','g','c','d']
+# print(arr[2])
+# arr[1] = 'e'
+# print(arr)
+# 从后面插入到列表中
+# arr.append('f')
+# print(arr)
+# 在第几位插入
+# arr.insert(0,'g')
+# print(arr)
+# 删除列表中的一项
+# del arr[0]
+# 弹出第几个
+# pop_out = arr.pop(1);
+# print(pop_out)
+# 移除第几个
+# remove_out = arr.remove('c')
+# print(remove_out)
+# 列表排序
+# arr.sort()
+# 列表倒叙排序
+# arr.sort(reverse=True)
+# print(len(arr))
+# 临时排序
+# print(arr)
+# print(sorted(arr));
+# address = ['beijing','shanghai','wenzhou','hangzhou']
+# print(address)
+# address.sort()
+# print(address)
+# print(sorted(address))
+# print(address)
+# print(address.sort())
+# print(address)
+# address.reverse()
+# print(len(address))
+
+# 第四章
+# for循环后跟缩进
+# arr = ['a','b','c','d']
+# for i in arr:
+# 	print(i)
+# 这样不会报错 i 为最后一项 也就是'd' i相当于js中的全局变量i
+# print(i)
+# print(1)
+# print(arr)
+# message = 'atie'
+# for i in message:
+# 	print(i)
+# a t i e
+# items = ['a','b','c']
+# for item in items:
+# 	print(item)
+# 范围 1打印到5
+# for num in range(1,6):
+# 	print(num)
+# list将range转化为列表
+# numbers = list(range(1,5))
+# print(numbers)
+# range可控制步数
+# for num in range(1,12,3):
+# 	print(num)
+# numbers = list(range(1,12,3))
+# print(numbers)
+# 求列表中最大数
+# print(max(numbers))
+# 求列表中最小数
+# print(min(numbers))
+# 求列表中数总和
+# print(sum(numbers))
+# abc = [value**2 for value in range(1,4)]
+# print(abc)
+
+# 切片(原列表数据不变)
+# arr = ['a','b','c']
+# print(arr[1:2])
+# 从0-1
+# print(arr[:1])
+# 从1-最后
+# print(arr[1:])
+# 最后第二个数起
+# print(arr[-2:])
+# print(arr)
+
+# 元组 （不可修改）
+# dimension = (200,50)
+# print(dimension[0])
+# print(dimension[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
