04e173a9efb71e8a42b11d2750e967b46a7a874d
zhoust <zhoust@guahao.com>
2018-05-15 00:11:48 +0800

    update

---
+++ practise/2018051301.py
@@ -0,0 +1,23 @@
+# -- coding: utf-8 --
+# if else
+# arr = ['a','b','c','d']
+# for i in arr:
+# 	if i == 'a':
+# 		print(0)
+# 	else:
+# 		print(1)
+# print('a' == 'A')
+# a = 4;
+# if a > 5:
+# 	print('a大于5')
+# elif a > 4:
+# 	print('a大于4')
+# else:
+# 	print('a不大于4')
+# a = False
+# print(a)
+# obj = {'name': 'atie','age': '18'}
+# del obj['name']
+# print(obj)
+# arr = ['a', 'r', 'g']
+# print(2)
---
+++ practise/2018051401.py
@@ -0,0 +1,89 @@
+# -- coding: utf-8 --
+# data = {
+# 	'name': 'zhou',
+# 	'age': '18'
+# }
+# for k,v in data.items():
+# 	print('key ====' + k)
+# 	print('value ====' + v)
+# for k in data:
+# 	print(k)
+# for k in data.keys():
+# 	print(k)
+# arr = [];
+# for index in range(5):
+# 	print(index)
+# 	arr.append(index)
+# print(arr)
+# for index in arr[:3]:
+# 	print(index)
+
+
+# 第七章
+# message = input('请输入你的名字');
+# print(message)
+# age = '18'
+# print(age > 17)
+
+# for index in range(20):
+# 	if index % 3 == 6:
+# 		break
+# 	else:
+# 	 print(index)
+
+#
+
+
+# 第八章 函数
+# def printName():
+# 	""" 描述注释 """
+# 	print('atie')
+# # printName()
+# def printName(name):
+# 	print(name)
+
+# printName("zhou")
+
+
+# def printFullName(first, last):
+# 	print("first" +first)
+# 	print('last'+last)
+
+# printFullName(last='atie',first='zhou')
+
+
+# def printNames(*arr):
+# 	print(arr)
+
+# printNames("atie")
+
+# def fn(a, **arr):
+# 	print(a)
+# 	print(arr)
+
+
+# fn(1,c=2)
+
+# def fn(a, *arr):
+# 	print(a)
+# 	print(arr)
+
+# fn(1,2,3,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
