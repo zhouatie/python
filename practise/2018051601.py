b781025ec9e8845463d99368e22d137d8f7e8e22
zhoust <zhoust@guahao.com>
2018-05-17 00:17:27 +0800

    1.搭建django基础环境

---
+++ practise/2018051601.py
@@ -0,0 +1,13 @@
+# import json
+# arr = [1,2,4,4,5]
+
+# filename = 'username.json'
+# with open(filename, 'w') as file_obj:
+# 	json.dump(arr, file_obj)
+
+# with open(filename) as file_obj:
+# 	numbers = json.load(file_obj)
+
+# print(numbers)
+
+# 学习django
