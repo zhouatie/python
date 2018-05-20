74e49c7c7d49c6302a9aa8e159bd256bd297e0f8
zhoust <zhoust@guahao.com>
2018-05-20 23:41:08 +0800

    update

--- project/flask-practice/app.py
+++ project/flask-practice/app.py
@@ -1,10 +1,27 @@
 # coding:utf8
-from flask import Flask
+import os
+from flask import Flask, render_template, request, session
+from werkzeug import secure_filename
+UPLOAD_FOLDER = './uploads'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def index():
     return "<h1 style='color:red'>hello world</h1>"
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('./hello.html', name=name)
+
+@app.route('/file', methods=['POST'])
+def file():
+    fileObj = request.files['file']
+    filename = secure_filename(fileObj.filename)
+    fileObj.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return '上传成功'
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
