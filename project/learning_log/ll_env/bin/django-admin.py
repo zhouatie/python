56632e1c66e4ba89ee8431cce493f63eb15e9c0f
zhoust <zhoust@guahao.com>
2018-05-27 01:52:39 +0800

    完成练习项目

--- project/learning_log/ll_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/zhouatie/Desktop/github/python/project/learning_log/ll_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
